d6fec6d97e71139054a775c24d2c4b9c460f32a4
masoo <m.goudarzi8933@gmail.com>
2024-02-08 22:39:56 +0330

    update main.js-add fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,66 @@
 let index = 0;
 
+// var Body = $('body');
 const totalWorkItems = $(".work-item").length;
 
 $(document).ready(function () {
-  $(window).on("load", function () {
-    $(".preloader").addClass("loaded");
-  });
-  
+  // $(window).on("load", function () {
+  //   $(".preloader").addClass("loaded");
+  // });
+
+  $(window).on('load', function () { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    // $('body').delay(350).css({'overflow':'visible'});
+  })
+
+  //   $(".nav-toggle").click(function () {
+  //     $(".header .nav").slideToggle();
+  // });
+
+  // $(".header .nav a").click(function (event) {
+  //   //if ($(window).width() < 768) {
+  //     $(".header .nav").slideToggle();
+  //     event.stopPropagation(); // Stop event propagation
+  //   //}
+  // });
+
+
+
+
+
+
+
+
   $(".nav-toggle").click(function () {
     $(".header .nav").slideToggle();
   })
-  
+
   $(".header .nav a").click(function () {
     if ($(window).width() < 768) {
       $(".header .nav").slideToggle();
     }
   });
 
-  
 
- 
 
-  
-  // end documen ready
+
+
+
+  // fixed header
+  $(window).scroll(function () {
+    if ((this).scrollTop() > 100) {
+      $(".header").addClass("fixed");
+    }
+    else {
+      $(".header").removeClass("fixed");
+    }
+  });
+
+
+
+
+
+
+   // end documen ready
 });
-
-
-
-
-
-
-
-
-
-
