3a890961345b1a87e14887fc825c94c8aa949b9b
masoo <m.goudarzi8933@gmail.com>
2024-02-10 19:57:58 +0330

    update main.js-add counterUp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,26 @@
 let index = 0;
 
-// var Body = $('body');
 const totalWorkItems = $(".work-item").length;
 
 $(document).ready(function () {
-  // $(window).on("load", function () {
-  //   $(".preloader").addClass("loaded");
-  // });
+
+  // preloader 
 
   $(window).on('load', function () { // makes sure the whole site is loaded 
     $('#status').fadeOut(); // will first fade out the loading animation 
     $('#preloader').fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    // $('body').delay(350).css({'overflow':'visible'});
+    $('body').delay(350).css({ 'overflow': 'visible' });
   })
 
-  //   $(".nav-toggle").click(function () {
-  //     $(".header .nav").slideToggle();
-  // });
-
-  // $(".header .nav a").click(function (event) {
-  //   //if ($(window).width() < 768) {
-  //     $(".header .nav").slideToggle();
-  //     event.stopPropagation(); // Stop event propagation
-  //   //}
-  // });
-
-
-
-
-
-
-
-
+  //navToggle mobile
   $(".nav-toggle").click(function () {
     $(".header .nav").slideToggle();
   })
-
   $(".header .nav a").click(function () {
     if ($(window).width() < 768) {
       $(".header .nav").slideToggle();
     }
   });
-
-
-
-
-
 
   // fixed header
   $(window).scroll(function () {
@@ -57,10 +32,118 @@ $(document).ready(function () {
     }
   });
 
+  
+  // counterUp 
+
+  $(function () {
+    function count($this) {
+      var current = parseInt($this.html(), 10);
+      current = current + 50; /* Where 50 is increment */
+
+      $this.html(++current);
+      if (current > $this.data('count')) {
+        $this.html($this.data('count'));
+      } else {
+        setTimeout(function () { count($this) }, 5);
+      }
+    }
+
+    $(".counter").each(function () {
+      $(this).data('count', parseInt($(this).html(), 10));
+      $(this).html('0');
+      count($(this));
+    });
+
+  });
+
+  
+  // slidebox
+  const wHeight = $(window).height();
+  $(".slidebox-img").css("max-height", wHeight + "px");
+
+  $(".work-item-inner").click(function () {
+    index = $(this).parent(".work-item").index();
+    $(".slidebox").addClass("open");
+    slideboxSlideShow();
+  });
+
+  $(".slidebox .prev").click(function () {
+    if (index == 0) {
+      index = totalWorkItems - 1;
+    } else {
+      index--;
+    }
+    slideboxSlideShow();
+  })
+  $(".slidebox .next").click(function () {
+    if (index == totalWorkItems - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    slideboxSlideShow();
+  });
+  
+  $(".slidebox-close").click(function () {
+    $(".slidebox").removeClass("open");
+  });
+
+  $(".slidebox").click(function (event) {
+    if ($(event.target).hasClass("slidebox")) {
+      $(this).removeClass("open");
+    }
+  });
+  
+  function slideboxSlideShow() {
+    const imgSrc = $(".work-item").eq(index).find("img").attr("data-large");
+    const category = $(".work-item").eq(index).find("h4").html();
+
+    $(".slidebox-img").attr("src", imgSrc);
+    $(".slidebox-category").html(category);
+    $(".slidebox-counter").html(totalWorkItems+"/"+(index+1));
+  };
+
+  // dialog
+
+  $( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#button" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
 
 
+  // social buttons
 
+   $( ".button-group > div" ).click(function() {
+  $('.button-group > div.active').not(this).removeClass('active');
+  $(this).toggleClass("active");
+});
+ $(".telegram").hover(function () {
+    $(this).toggleClass("color-telegram");
+});
+$(".linkedin").hover(function () {
+  $(this).toggleClass("color-linkedin");
+});
 
+$(".whatsapp").hover(function () {
+  $(this).toggleClass("color-whatsapp")
+});
 
-   // end documen ready
+$(".instagram").hover(function () {
+  $(this).toggleClass("color-instagram")
+});
+  
+  // end documen ready
 });
